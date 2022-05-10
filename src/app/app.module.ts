3573ef4a4a1ee14d22737d6a18c64c084a36f217
Thais B <thais_brt@hotmail.com>
2022-05-10 15:25:28 -0300

    criando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { TopBarComponent } from './header/top-bar/top-bar.component';
+import { HeaderMainComponent } from './header/header-main/header-main.component';
+import { MainNavigationComponent } from './header/main-navigation/main-navigation.component';
+import { MenuComponent } from './header/menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { SidebarComponent } from './shop/sidebar/sidebar.component';
+import { ShopContentComponent } from './shop/shop-content/shop-content.component';
+import { ViewedComponent } from './viewed/viewed.component';
+import { BrandsComponent } from './brands/brands.component';
+import { SliderComponent } from './brands/slider/slider.component';
+import { NewsletterComponent } from './newsletter/newsletter.component';
+import { FooterComponent } from './footer/footer.component';
+import { CopyrightComponent } from './copyright/copyright.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TopBarComponent,
+    HeaderMainComponent,
+    MainNavigationComponent,
+    MenuComponent,
+    HomeComponent,
+    ShopComponent,
+    SidebarComponent,
+    ShopContentComponent,
+    ViewedComponent,
+    BrandsComponent,
+    SliderComponent,
+    NewsletterComponent,
+    FooterComponent,
+    CopyrightComponent
   ],
   imports: [
     BrowserModule,
